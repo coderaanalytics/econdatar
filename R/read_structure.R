5cbd4c850e76d8898cd6b15d15d03f9d7870fc25
byron <byron@byrongibby.online>
2024-01-23 13:09:37 +0200

    Updated reading/writing structures

--- R/read_structure.R
+++ R/read_structure.R
@@ -54,9 +54,9 @@ read_structure <- function(structure, id, ...) {
 
   structures <- lapply(structure_data, function(x) {
     switch(structure,
-           "codelist" = process_codelist(x, file),
-           "concept-scheme" = process_concept_scheme(x, file),
-           "data-structure" = process_data_structure(x, file),
+           "codelist" = process_codelist(x),
+           "concept-scheme" = process_concept_scheme(x),
+           "data-structure" = process_data_structure(x),
            stop("Specified structure, ", structure, ", is not supported."))
   })
 
@@ -73,7 +73,7 @@ read_structure <- function(structure, id, ...) {
 
 
 read_codelists <- function(env, agencyids, ids, versions) {
-  message(paste("\nFetching data set(s) -", paste(ids, collapse = ", "), "\n"))
+  message(paste("\nFetching codelist(s) -", paste(ids, collapse = ", "), "\n"))
 
   response <- GET(env$registry$url,
                   path = paste(c(env$registry$path, "codelists"), collapse = "/"),
@@ -92,13 +92,13 @@ read_codelists <- function(env, agencyids, ids, versions) {
   return(codelists)
 }
 
-process_codelist <- function(structure, file) {
+process_codelist <- function(structure) {
   structure_ref <- paste(structure[[2]]$agencyid, 
                          structure[[2]]$id,
                          structure[[2]]$version,
                          sep = "-")
 
-  message("Processing structure: ", structure_ref, "\n")
+  message("Processing codelist: ", structure_ref, "\n")
 
   description <- if (is.null(structure[[2]]$description[[2]])) {
     NA
@@ -125,11 +125,6 @@ process_codelist <- function(structure, file) {
     }) |>
     do.call(rbind.data.frame, args = _)
 
-  if(!is.null(file)) {
-    write_ods(as.data.frame(codelist), path = file, sheet = "codelist")
-    write_ods(codes, path = file, sheet = "codes", append = TRUE)
-  }
-
   codelist$codes <- codes
   return(codelist)
 }
@@ -140,11 +135,74 @@ process_codelist <- function(structure, file) {
 
 
 read_concept_schemes <- function(env, agencyids, ids, versions) {
-  stop("Not implemented")
+  message(paste("\nFetching concept scheme(s) -", paste(ids, collapse = ", "), "\n"))
+
+  response <- GET(env$registry$url,
+                  path = paste(c(env$registry$path, "conceptschemes"), collapse = "/"),
+                  query = list(agencyids = agencyids, ids = ids, versions = versions),
+                  set_cookies(.cookies = get("econdata_session", envir = .pkgenv)),
+                  accept("application/vnd.sdmx-codera.data+json"))
+
+  if (response$status_code != 200) {
+    stop(content(response, encoding = "UTF-8"))
+  }
+
+  data_message <- content(response, type = "application/json", encoding = "UTF-8")
+
+  concept_schemes <- data_message[[2]][["structures"]][["concept-schemes"]]
+
+  return(concept_schemes)
 }
 
-process_concept_scheme <- function(structure, file) {
-  stop("Not implemented")
+process_concept_scheme <- function(structure) {
+  structure_ref <- paste(structure[[2]]$agencyid, 
+                         structure[[2]]$id,
+                         structure[[2]]$version,
+                         sep = "-")
+
+  message("Processing concept scheme: ", structure_ref, "\n")
+
+  description <- if (is.null(structure[[2]]$description[[2]])) {
+    NA
+  } else {
+    structure[[2]]$description[[2]]
+  }
+
+  concept_scheme <- list(agencyid = structure[[2]]$agencyid,
+                         id = structure[[2]]$id,
+                         version = structure[[2]]$version,
+                         name = structure[[2]]$name[[2]],
+                         description = description)
+
+  concepts <- lapply(structure[[2]]$concepts, function(concept) {
+      description <- if (is.null(concept[[2]]$description[[2]])) {
+        NA
+      } else {
+        concept[[2]]$description[[2]]
+      }
+
+      representation <- if (is.list(concept[[2]][["core-representation"]])) {
+        codelist_ref <- component[[2]][["core-representation"]][[2]]
+        list(representation = "codelist",
+             codelist_agencyid = codelist_ref$agencyid,
+             codelist_id = codelist_ref$id,
+             codelist_version = codelist_ref$version)
+      } else {
+        list(representation = concept[[2]][["core-representation"]],
+             codelist_agencyid = NA,
+             codelist_id = NA,
+             codelist_version = NA)
+      }
+
+      c(list(id = concept[[2]]$id,
+             name = concept[[2]]$name[[2]],
+             description = description),
+        representation)
+    }) |>
+    do.call(rbind.data.frame, args = _)
+
+  concept_scheme$concepts <- concepts
+  return(concept_scheme)
 }
 
 
@@ -153,9 +211,149 @@ process_concept_scheme <- function(structure, file) {
 
 
 read_data_structures <- function(env, agencyids, ids, versions) {
-  stop("Not implemented")
+  message(paste("\nFetching data structure(s) -", paste(ids, collapse = ", "), "\n"))
+
+  response <- GET(env$registry$url,
+                  path = paste(c(env$registry$path, "datastructures"), collapse = "/"),
+                  query = list(agencyids = agencyids, ids = ids, versions = versions),
+                  set_cookies(.cookies = get("econdata_session", envir = .pkgenv)),
+                  accept("application/vnd.sdmx-codera.data+json"))
+
+  if (response$status_code != 200) {
+    stop(content(response, encoding = "UTF-8"))
+  }
+
+  data_message <- content(response, type = "application/json", encoding = "UTF-8")
+
+  data_structures <- data_message[[2]][["structures"]][["data-structures"]]
+
+  return(data_structures)
 }
 
-process_data_structure <- function(structure, file) {
-  stop("Not implemented")
+process_data_structure <- function(structure) {
+  structure_ref <- paste(structure[[2]]$agencyid, 
+                         structure[[2]]$id,
+                         structure[[2]]$version,
+                         sep = "-")
+
+  message("Processing data structure: ", structure_ref, "\n")
+
+  description <- if (is.null(structure[[2]]$description[[2]])) {
+    NA
+  } else {
+    structure[[2]]$description[[2]]
+  }
+
+  data_structure <- list(agencyid = structure[[2]]$agencyid,
+                         id = structure[[2]]$id,
+                         version = structure[[2]]$version,
+                         name = structure[[2]]$name[[2]],
+                         description = description)
+
+  dimensions <- lapply(structure[[2]]$components, function(component) {
+      if (component[[1]] == "#sdmx.infomodel.datastructure.Dimension") {
+        concept_ref <- component[[2]][["concept-identity"]][[2]]
+        concept <- list(concept_agencyid = concept_ref$agencyid,
+                        concept_parentid = concept_ref$parentid,
+                        concept_parentversion = concept_ref$parentversion,
+                        concept_id = concept_ref$id)
+
+        representation <- if (is.list(component[[2]][["local-representation"]])) {
+          codelist_ref <- component[[2]][["local-representation"]][[2]]
+          list(representation = "codelist",
+               codelist_agencyid = codelist_ref$agencyid,
+               codelist_id = codelist_ref$id,
+               codelist_version = codelist_ref$version)
+        } else {
+          list(representation = component[[2]][["local-representation"]],
+               codelist_agencyid = NA,
+               codelist_id = NA,
+               codelist_version = NA)
+        }
+
+        c(list(id = component[[2]]$id,
+               position = component[[2]]$position),
+          concept,
+          representation)
+      }
+    }) |>
+    do.call(rbind.data.frame, args = _)
+
+  dimensions_ordered <- dimensions[order(dimensions$position),]
+  rownames(dimensions_ordered) <- NULL
+
+  attrs <- lapply(structure[[2]]$components, function(component) {
+      if (component[[1]] == "#sdmx.infomodel.datastructure.Attribute") {
+        concept_ref <- component[[2]][["concept-identity"]][[2]]
+        concept <- list(concept_agencyid = concept_ref$agencyid,
+                        concept_parentid = concept_ref$parentid,
+                        concept_parentversion = concept_ref$parentversion,
+                        concept_id = concept_ref$id)
+
+        representation <- if (is.list(component[[2]][["local-representation"]])) {
+          codelist_ref <- component[[2]][["local-representation"]][[2]]
+          list(representation = "codelist",
+               codelist_agencyid = codelist_ref$agencyid,
+               codelist_id = codelist_ref$id,
+               codelist_version = codelist_ref$version)
+        } else {
+          list(representation = component[[2]][["local-representation"]],
+               codelist_agencyid = NA,
+               codelist_id = NA,
+               codelist_version = NA)
+        }
+
+        c(list(id = component[[2]]$id,
+               level = component[[2]][["attachment-level"]],
+               mandatory = component[[2]][["assignment-mandatory"]]),
+          concept,
+          representation)
+      }
+    }) |>
+    do.call(rbind.data.frame, args = _)
+
+  attrs_ordered <- attrs[order(attrs$level),]
+  rownames(attrs_ordered) <- NULL
+
+  time_dimension <- lapply(structure[[2]]$components, function(component) {
+      if (component[[1]] == "#sdmx.infomodel.datastructure.TimeDimension") {
+        concept_ref <- component[[2]][["concept-identity"]][[2]]
+        concept <- list(concept_agencyid = concept_ref$agencyid,
+                        concept_parentid = concept_ref$parentid,
+                        concept_parentversion = concept_ref$parentversion,
+                        concept_id = concept_ref$id)
+
+        representation <- list(representation = component[[2]][["local-representation"]])
+
+        c(list(id = component[[2]]$id),
+          concept,
+          representation)
+      }
+    }) |>
+    do.call(rbind.data.frame, args = _)
+
+  primary_measure <- lapply(structure[[2]]$components, function(component) {
+      if (component[[1]] == "#sdmx.infomodel.datastructure.PrimaryMeasure") {
+        concept_ref <- component[[2]][["concept-identity"]][[2]]
+        concept <- list(concept_agencyid = concept_ref$agencyid,
+                        concept_parentid = concept_ref$parentid,
+                        concept_parentversion = concept_ref$parentversion,
+                        concept_id = concept_ref$id)
+
+        representation <- list(representation = component[[2]][["local-representation"]])
+        
+        c(list(id = component[[2]]$id),
+          concept,
+          representation)
+      }
+    }) |>
+    do.call(rbind.data.frame, args = _)
+
+  components <- list(dimensions = dimensions_ordered,
+                     attributes = attrs_ordered,
+                     primary_measure = primary_measure,
+                     time_dimension = time_dimension)
+
+  data_structure$components <- components
+  return(data_structure)
 }
--- R/write_structure.R
+++ R/write_structure.R
@@ -1,3 +1,84 @@
-write_structure <- function(...) {
-  stop("Not implemented")
+write_structure <- function(structure, x, ...) {
+
+
+  # Parameters ---
+
+  env <- fromJSON(system.file("settings.json", package = "econdatar"))
+
+  params <- list(...)
+
+  if (!is.null(params$username) && !is.null(params$password)) {
+    credentials <- paste(params$username, params$password, sep = ";")
+  } else {
+    credentials <- NULL
+  }
+
+  if (!is.null(params$file)) {
+    file  <- params$file
+  } else {
+    file <- NULL
+  }
+
+
+  # Fetch structure(s) ---
+
+  if (!exists("econdata_session", envir = .pkgenv)) {
+    login_helper(credentials, env$repository$url)
+  }
+
+  structure_data <-
+    switch(structure,
+           "codelist" = write_codelist(x, env, file),
+           "concept-scheme" = write_concept_scheme(x, env, file),
+           "data-structure" = write_data_structure(x, env, file),
+           stop("Specified structure, ", structure, ", is not supported."))
+}
+
+
+
+# Codelist ---
+
+write_codelist <- function(codelist, env, file) {
+  if(!is.null(file)) {
+    codes <- codelist$codes
+    codelist$codes <- NULL
+    write_ods(as.data.frame(codelist), path = file, sheet = "codelist")
+    write_ods(codes, path = file, sheet = "codes", append = TRUE)
+    message("Codelist successfully written to: ", file, "\n")
+  }
+}
+
+
+# Concept scheme ---
+
+
+write_concept_scheme <- function(concept_scheme, env, file) {
+  if(!is.null(file)) {
+    concepts <- concept_scheme$concepts
+    concept_scheme$concepts <- NULL
+    write_ods(as.data.frame(concept_scheme), path = file, sheet = "concept_scheme")
+    write_ods(concepts, path = file, sheet = "concepts", append = TRUE)
+    message("Concept scheme successfully written to: ", file, "\n")
+  }
+}
+
+
+
+# Data structure ---
+
+
+write_data_structure <- function(data_structure, env, file) {
+  if(!is.null(file)) {
+    dimensions <- data_structure$components$dimensions
+    attrs <- data_structure$components$attributes
+    time_dimension <- data_structure$components$time_dimension
+    primary_measure <- data_structure$components$primary_measure
+    data_structure$components <- NULL
+    write_ods(as.data.frame(data_structure), path = file, sheet = "data_structure")
+    write_ods(dimensions, path = file, sheet = "dimenions", append = TRUE)
+    write_ods(attrs, path = file, sheet = "attributes", append = TRUE)
+    write_ods(time_dimension, path = file, sheet = "time_dimension", append = TRUE)
+    write_ods(primary_measure, path = file, sheet = "primary_measure", append = TRUE)
+    message("Data structure successfully written to: ", file, "\n")
+  }
 }
