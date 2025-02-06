02eb986097719ad91f6c18d100d8ca32dc899233
byron <byron@byrongibby.online>
2025-02-06 14:51:21 +0200

    Revert "Enter API key via terminal if DISPLAY is unavailable"
    This reverts commit 7afd1a438b8b06425c30bfcd0001977bf54c73e5.

--- R/econdata_credentials.R
+++ R/econdata_credentials.R
@@ -4,33 +4,29 @@ econdata_credentials <- function() {
          call. = FALSE)
   }
   tkn <- NULL # Need to add global bindings for variables
-  if (Sys.getenv("DISPLAY") != "") {
-    token   <- tcltk::tclVar("")
+  token   <- tcltk::tclVar("")
 
-    tt <- tcltk::tktoplevel()
-    tcltk::tkwm.title(tt, "econdata.co.za credentials")
-    tkn.entry <- tcltk::tkentry(tt, textvariable = token)
+  tt <- tcltk::tktoplevel()
+  tcltk::tkwm.title(tt, "econdata.co.za credentials")
+  tkn.entry <- tcltk::tkentry(tt, textvariable = token)
 
-    reset <- function() tcltk::tclvalue(token) <- ""
-    reset.but <- tcltk::tkbutton(tt, text = "Reset", command = reset)
+  reset <- function() tcltk::tclvalue(token) <- ""
+  reset.but <- tcltk::tkbutton(tt, text = "Reset", command = reset)
 
-    submit <- function() {
-        tkn <- tcltk::tclvalue(token)
-        e <- parent.env(environment())
-        e$tkn <- tkn
-        tcltk::tkdestroy(tt)
-    }
-    submit.but <- tcltk::tkbutton(tt, text = "Submit", command = submit)
-
-    tcltk::tkgrid(tcltk::tklabel(tt, text = "Enter Token Details"),
-                columnspan = 2)
-    tcltk::tkgrid(tcltk::tklabel(tt, text = "API Token"),
-                tkn.entry, pady = 10, padx = 10)
-    tcltk::tkgrid(submit.but, reset.but, pady = 10, padx = 50)
-
-    tcltk::tkwait.window(tt)
-  } else {
-    tkn <- readline("Enter API key: ")
+  submit <- function() {
+    tkn <- tcltk::tclvalue(token)
+    e <- parent.env(environment())
+    e$tkn <- tkn
+    tcltk::tkdestroy(tt)
   }
+  submit.but <- tcltk::tkbutton(tt, text = "Submit", command = submit)
+
+  tcltk::tkgrid(tcltk::tklabel(tt, text = "Enter Token Details"),
+                columnspan = 2)
+  tcltk::tkgrid(tcltk::tklabel(tt, text = "API Token"),
+                tkn.entry, pady = 10, padx = 10)
+  tcltk::tkgrid(submit.but, reset.but, pady = 10, padx = 50)
+
+  tcltk::tkwait.window(tt)
   return(tkn)
 }
