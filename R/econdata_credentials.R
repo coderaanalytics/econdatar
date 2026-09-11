ac6961291c05566cda048f25f8ec670bfb1f3e93
byron <byron@byrongibby.online>
2026-09-11 13:21:40 +0200

    Add CLI prompt fallback in the case that tcltk fails
    This is especially important to users running in headless environments.

--- R/econdata_credentials.R
+++ R/econdata_credentials.R
@@ -1,32 +1,37 @@
 econdata_credentials <- function() {
-  if (!requireNamespace("tcltk", quietly = TRUE)) {
-    stop("Package \"tcltk\" needed for this function to work.",
-         call. = FALSE)
-  }
   tkn <- NULL # Need to add global bindings for variables
-  token   <- tcltk::tclVar("")
 
-  tt <- tcltk::tktoplevel()
-  tcltk::tkwm.title(tt, "econdata.co.za credentials")
-  tkn.entry <- tcltk::tkentry(tt, textvariable = token)
+  tryCatch({
+    token   <- tcltk::tclVar("")
 
-  reset <- function() tcltk::tclvalue(token) <- ""
-  reset.but <- tcltk::tkbutton(tt, text = "Reset", command = reset)
+    tt <- tcltk::tktoplevel()
+    tcltk::tkwm.title(tt, "econdata.co.za credentials")
+    tkn.entry <- tcltk::tkentry(tt, textvariable = token)
 
-  submit <- function() {
-    tkn <- tcltk::tclvalue(token)
-    e <- parent.env(environment())
-    e$tkn <- tkn
-    tcltk::tkdestroy(tt)
+    reset <- function() tcltk::tclvalue(token) <- ""
+    reset.but <- tcltk::tkbutton(tt, text = "Reset", command = reset)
+
+    submit <- function() {
+      tkn <- tcltk::tclvalue(token)
+      e <- parent.env(environment())
+      e$tkn <- tkn
+      tcltk::tkdestroy(tt)
+    }
+    submit.but <- tcltk::tkbutton(tt, text = "Submit", command = submit)
+
+    tcltk::tkgrid(tcltk::tklabel(tt, text = "Enter Token Details"),
+                  columnspan = 2)
+    tcltk::tkgrid(tcltk::tklabel(tt, text = "API Token"),
+                  tkn.entry, pady = 10, padx = 10)
+    tcltk::tkgrid(submit.but, reset.but, pady = 10, padx = 50)
+
+    tcltk::tkwait.window(tt)
+  }, error = function(e) {
+  })
+
+  if (is.null(tkn) || identical(tkn, "")) {
+    tkn <- readline(prompt = "Please provide EconData token: ")
   }
-  submit.but <- tcltk::tkbutton(tt, text = "Submit", command = submit)
 
-  tcltk::tkgrid(tcltk::tklabel(tt, text = "Enter Token Details"),
-                columnspan = 2)
-  tcltk::tkgrid(tcltk::tklabel(tt, text = "API Token"),
-                tkn.entry, pady = 10, padx = 10)
-  tcltk::tkgrid(submit.but, reset.but, pady = 10, padx = 50)
-
-  tcltk::tkwait.window(tt)
   return(tkn)
 }
